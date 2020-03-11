766168a34cdda3ca4716f7a002664ddd1ff7bb63
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-03-11 11:42:01 -0400

    Routed forms table to applications table

--- public/js/applications.js
+++ public/js/applications.js
@@ -1,39 +1,21 @@
-let applications = [
-  {
-    "status": "NEW",
-    "textInput": "Svetlana",
-    "textArea": "I'm applying because...",
-    "checkBox": true,
-    "createdAt": "2020-03-10",
-    "date": "2020-03-12"
-  },
-  {
-    "status": "UNDER_REVIEW",
-    "textInput": "Stan",
-    "textArea": "I'm the best...",
-    "checkBox": true,
-    "createdAt": "2020-03-11",
-    "date": "2020-03-11"
-  },
-  {
-    "status": "UNDER_REVIEW",
-    "textInput": "Random Guy",
-    "textArea": "I'm Random...",
-    "checkBox": true,
-    "createdAt": "2020-03-11",
-    "date": "2020-03-11"
-  }
-];
+fetch('/forms')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    applications = data;
+    console.log(data);
 
-$(document).ready(function() {
-  let table = $("#applications-table");
+    $(document).ready(function() {
+      let table = $('#applications-table');
 
-  table.DataTable ({
-      "data" : applications,
-      "columns" : [
-          { "data" : "textInput" },
-          { "data" : "status" },
-          { "data" : "createdAt" }
-      ]
+      table.DataTable({
+        data: applications,
+        columns: [
+          { data: 'textInput' },
+          { data: 'status' },
+          { data: 'createdAt' }
+        ]
+      });
+    });
   });
-});

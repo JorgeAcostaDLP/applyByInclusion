f09330d1ab23da5059b786b0349336b4ad5d5249
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-03-11 15:19:35 -0400

    added ID field that now routes to single form, super hacky

--- public/js/applications.js
+++ public/js/applications.js
@@ -4,20 +4,28 @@ fetch('/forms')
   })
   .then(data => {
     applications = data;
-    console.log(data);
-
     $(document).ready(function() {
       let table = $('#applications-table');
-
+      console.log(applications);
       table.DataTable({
         data: applications,
         columns: [
+          { data: 'id' },
           {
             data: 'textInput'
           },
           { data: 'status' },
           { data: 'createdAt' }
         ]
+      });
+      $(document).on('click', '#applications-table tr', function(e) {
+        e.preventDefault();
+        currentTarget = e.currentTarget.innerHTML;
+        formId = currentTarget.slice(
+          currentTarget.indexOf('>') + 1,
+          currentTarget.indexOf('</')
+        );
+        $(location).attr('href', `/forms/` + formId);
       });
     });
   });

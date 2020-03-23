14f3431c15b8041a47fb66ddac23cccb541df090
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-03-23 15:15:16 -0400

    added a form to login, checks if the username and password submitted exist

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,34 @@
-let admins = {};
+let admins;
 fetch('/admins/')
   .then(response => {
     return response.json();
   })
   .then(data => {
     admins = data;
+    console.log(admins);
   });
 
-document.getElementById('login').addEventListener('click', handleSubmit);
+document.getElementById('login').addEventListener('click', handleSubmitMain);
 
-function handleSubmit(e) {
+function handleSubmitMain(e) {
   e.preventDefault();
+  document.getElementById('loginForm').style.display = 'block';
+}
+
+document
+  .getElementById('loginSubmit')
+  .addEventListener('click', handleSubmitLogin);
+
+function handleSubmitLogin(e) {
+  e.preventDefault();
+  let userName = document.getElementById('userName').value;
+  let passWord = document.getElementById('passWord').value;
+  let loginSuccess = false;
+  admins.forEach(element => {
+    if (element.userName === userName && element.userPassword === passWord)
+      loginSuccess = true;
+  });
+  if (loginSuccess)
+    document.location.href = document.location.href + '/applications.html';
+  else alert('Login Unsuccesful');
 }
